f0d3fb4ac33ac6be47cb8dd92bf290bd9c9cb7af
Denis <Denis-life@yandex.ru>
2020-04-25 15:46:28 +0300

    .

--- Chapter07/myshop/cart/cart.py
+++ Chapter07/myshop/cart/cart.py
@@ -7,12 +7,12 @@ class Cart(object):
 
     def __init__(self, request):
         """
-        Initialize the cart.
+        Инициализируем корзину
         """
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
-            # save an empty cart in the session
+            # сохраняем в сессии пустую корзину
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
